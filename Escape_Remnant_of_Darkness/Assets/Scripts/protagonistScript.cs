f4d1486240d9b45383d10fa3931123c39e28b05e
Pravdin Maksym <maks111111112@gmail.com>
2021-04-02 18:05:20 -0400

    Ajout de blend tree et d'animations pour le protagoniste

---
+++ Escape_Remnant_of_Darkness/Assets/Scripts/protagonistScript.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class protagonistScript : MonoBehaviour
+{
+    public float moveSpeed = 2.4f;
+
+    public Rigidbody2D rb;
+
+    public Vector2 movement;
+
+    public Animator animator;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement.x = Input.GetAxis("Horizontal");
+        movement.y = Input.GetAxis("Vertical");
+        movement.Normalize();
+
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("Speed", movement.sqrMagnitude);
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+}
