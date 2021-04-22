4fef31ee47f420d5da8ca9c793ee346a56303ad9
Renaud Vincent <v.pisano35@gmail.com>
2021-04-22 17:03:04 -0400

    Safe Zone v.2

---
+++ Escape_Remnant_of_Darkness/Assets/Editor/ProtagonistEditor.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+
+[CustomEditor(typeof(ProtagonistScript))]
+public class ProtagonistEditor : Editor
+{
+    private void OnSceneGUI()
+    {
+        ProtagonistScript _protagonistScript = (ProtagonistScript) target;
+        Handles.color = Color.white;
+        Handles.DrawWireArc(_protagonistScript.transform.position, Vector3.forward, Vector3.right,
+            360, _protagonistScript.GetViewRadius());
+
+    }
+}
--- Escape_Remnant_of_Darkness/Assets/Scripts/ProtagonistScripts/ProtagonistScript.cs
+++ Escape_Remnant_of_Darkness/Assets/Scripts/ProtagonistScripts/ProtagonistScript.cs
@@ -5,11 +5,14 @@ using UnityEngine.Experimental.Rendering.Universal;
 
 public class ProtagonistScript : MonoBehaviour
 {
-    private float MIN_LIGHT_INTENSITY = 0.2f;
-    
+    private readonly float MIN_LIGHT_INTENSITY = 0.2f;
+    private float MAX_SANITY;
+    private float MAX_LIGHT_INTENSITY;
+
     [SerializeField][Range(0,5)] private float moveSpeed = 2.4f;
     [SerializeField] [Range(0, 300)] private float lightDurationInSeconds = 20f; 
     [SerializeField] [Range(0, 2)] private float lightFallOff = 0.2f; 
+    [SerializeField] [Range(0, 10)] private float viewRadius = 0.2f; 
     
     //Movements
     private Rigidbody2D _rigidbody;
@@ -22,9 +25,11 @@ public class ProtagonistScript : MonoBehaviour
     private Light2D _light;
     private float _timer;
     private float _intensityPerSeconds;
+    private LayerMask _targetMask;
 
     //Madness
     [SerializeField] [Range(0, 400)] private float sanity = 300f;
+    private bool _isPlayerInSafeZone;
     
     //SpeedBoosting
     private float memSpeed;
@@ -37,17 +42,24 @@ public class ProtagonistScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        //Movements
         _rigidbody = GetComponent<Rigidbody2D>();
         _animator = GetComponent<Animator>();
-
-        memSpeed = moveSpeed;
 
         //Lights
         _pointLight = transform.GetChild(0).gameObject;
         _light = _pointLight.GetComponent<Light2D>();
+        MAX_LIGHT_INTENSITY = _light.intensity;
         _intensityPerSeconds = (_light.intensity - MIN_LIGHT_INTENSITY) / lightDurationInSeconds * lightFallOff;
-        StartCoroutine("Light", lightFallOff);
+        _targetMask = LayerMask.GetMask("LightSource");
+        
+        MAX_SANITY = sanity;
+        
+        //Coroutines
+        StartCoroutine("ManageLight", lightFallOff);
+        StartCoroutine("FindLightSources", .2f);
+
+        memSpeed = moveSpeed;
+        
     }
 
     // Update is called once per frame
@@ -61,21 +73,7 @@ public class ProtagonistScript : MonoBehaviour
         
             AnimateMovement();
 
-            if (boosting)
-            {
-                speedDuration -= Time.deltaTime;
-                if (speedDuration <= 0)
-                {
-                    moveSpeed = memSpeed;
-                    speedDuration = 1.5f;
-                    boosting = false;
-                }
-                else
-                {
-                    moveSpeed = memSpeed + bonusSpeed;
-                }
-                
-            }
+            CheckAfterDamageBoost();
         }
         else
         {
@@ -85,6 +83,24 @@ public class ProtagonistScript : MonoBehaviour
             _animator.SetBool("Vanquished", true);
         }
         
+    }
+
+    private void CheckAfterDamageBoost()
+    {
+        if (boosting)
+        {
+            speedDuration -= Time.deltaTime;
+            if (speedDuration <= 0)
+            {
+                moveSpeed = memSpeed;
+                speedDuration = 1.5f;
+                boosting = false;
+            }
+            else
+            {
+                moveSpeed = memSpeed + bonusSpeed;
+            }
+        }
     }
 
     private void AnimateMovement()
@@ -111,19 +127,44 @@ public class ProtagonistScript : MonoBehaviour
         _rigidbody.MovePosition(_rigidbody.position + _velocity * (moveSpeed * Time.fixedDeltaTime));
     }
     
-    public IEnumerator Light(float delay)
+    public IEnumerator FindLightSources(float delay)
     {
         while (true)
         {
             yield return new WaitForSeconds(delay);
-            if (_timer < lightDurationInSeconds)
+            Collider2D[] targetsInViewRadius = Physics2D.OverlapCircleAll(transform.position, viewRadius, _targetMask);
+            if (targetsInViewRadius.Length > 0)
             {
-                _light.intensity -= _intensityPerSeconds;
-                if (_light.intensity < MIN_LIGHT_INTENSITY)
-                    _light.intensity = MIN_LIGHT_INTENSITY;
-                _timer += delay;
+                _isPlayerInSafeZone = true;
             }
-            ReduceSanity();
+            else
+                _isPlayerInSafeZone = false;
+        }
+    }
+    
+    public IEnumerator ManageLight(float delay)
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(delay);
+            if (_isPlayerInSafeZone)
+            {
+                if (_light.intensity < MAX_LIGHT_INTENSITY)
+                    _light.intensity += _intensityPerSeconds;
+                if (sanity < MAX_SANITY)
+                    sanity++;
+            }
+            else
+            {
+                if (_timer < lightDurationInSeconds)
+                {
+                    _light.intensity -= _intensityPerSeconds;
+                    if (_light.intensity < MIN_LIGHT_INTENSITY)
+                        _light.intensity = MIN_LIGHT_INTENSITY;
+                    _timer += delay;
+                }
+                ReduceSanity();
+            }
         }
     }
 
@@ -156,6 +197,11 @@ public class ProtagonistScript : MonoBehaviour
     public float GetLightIntensity()
     {
         return _light.intensity - MIN_LIGHT_INTENSITY;
+    }
+    
+    public float GetViewRadius()
+    {
+        return viewRadius;
     }
 
     private void OnCollisionEnter2D(Collision2D other)
